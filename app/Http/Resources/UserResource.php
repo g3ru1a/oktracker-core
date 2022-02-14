1150fee88532770b2813d6ed945f9cfc72a4a769
G3ru1a <egidiufarcas@maze.ws>
2022-02-14 17:11:16 +0000

    added pfp to UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,10 +14,16 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
+        if ($this->profile_photo_path != null) {
+            $pfp_path = env("APP_URL") . $this->profile_photo_path;
+        }else {
+            $pfp_path = "https://media.discordapp.net/attachments/825042681779716136/940698494094692362/default_pfp.png";
+        }
         return [
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
+            "pfp" => $pfp_path,
             'role' => RoleResource::make($this->role),
             'badges' => $this->badges
         ];
