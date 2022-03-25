d4415062a2eac69b295139c34eafbf2bce2fab4e
g3ru1a <egidiufarcas@maze.ws>
2022-03-25 21:33:15 +0000

    patch

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('dashboard'));
 });
 
 Route::get('/policy', function () {
