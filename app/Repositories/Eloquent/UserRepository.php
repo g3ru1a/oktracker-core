88f4be004170f85d3f85bf42883ed9b725871a19
G3ru1a <egidiufarcas@maze.ws>
2022-07-28 13:47:36 +0100

    patched type error

--- app/Repositories/Eloquent/UserRepository.php
+++ app/Repositories/Eloquent/UserRepository.php
@@ -50,7 +50,7 @@ class UserRepository extends BaseRepository implements UserRepositoryInterface
      * @param string $email
      * @return User|null
      */
-    public function findByEmail($email): User
+    public function findByEmail($email): User|null
     {
         $user = User::where('email', $email)->first();
         return $user;
